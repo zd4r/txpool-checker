eeb71d2994498f7f8b7d271ae85325e958367944
zd4rova <fakenotbad75@gmail.com>
2023-01-04 20:55:52 +0300

    fix drop replaced txs

--- cmd/api/replaced_txs.go
+++ cmd/api/replaced_txs.go
@@ -19,6 +19,7 @@ func (p *txpoolGlobal) dropReplacedPendingTxs(wg *sync.WaitGroup) {
 			return true
 		}
 
+		log.Printf("[%v] CHECK %v\n", p.config.uuid, txHash)
 		tx, _, err := p.config.ethClientHTTPS.TransactionByHash(context.Background(), common.HexToHash(txHash[:2]))
 		if err != nil {
 			log.Printf("[%v] %v\n", p.config.uuid, fmt.Errorf("transactionByHash error: %v", err))
